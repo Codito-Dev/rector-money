71a94daf226c421c08b43f1bcd44b05c4a434808
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-19 20:41:42 +0700

    Bump to Rector 0.19.2

--- src/Rule/CurrencyAvailableWithinToCurrenciesContainsRector.php
+++ src/Rule/CurrencyAvailableWithinToCurrenciesContainsRector.php
@@ -8,7 +8,7 @@ use Money\Currency;
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Type\TypeWithClassName;
-use Rector\Core\Rector\AbstractRector;
+use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
